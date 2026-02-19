3a9ec67e9903284678eb19e5b77f7be76e695144
Pascal Schiessle <pascal.schiessle@web.de>
2026-02-19 01:27:31 +0100

    [feat]Init test project with xUnit

---
+++ LumenForgeServer/LumenForgeServer.IntegrationTests/HealthTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace LumenForgeServer.IntegrationTests;
+
+public class HealthTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+
+    public HealthTests(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient(); // base address is set automatically
+    }
+
+    [Fact]
+    public async Task GET_health_returns_ok()
+    {
+        var resp = await _client.GetAsync("/health");
+
+        resp.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var json = await resp.Content.ReadFromJsonAsync<Dictionary<string, object>>();
+        json.Should().ContainKey("status");
+    }
+}
