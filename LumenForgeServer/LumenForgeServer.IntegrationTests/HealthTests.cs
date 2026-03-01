88a1ffc4b9f7a78939cf97c4a9a9b6c68635f2d0
Pascal Schiessle <pascal.schiessle@web.de>
2026-03-01 20:29:26 +0100

    [ref] Fixed Healthtest

--- LumenForgeServer/LumenForgeServer.IntegrationTests/HealthTests.cs
+++ LumenForgeServer/LumenForgeServer.IntegrationTests/HealthTests.cs
@@ -4,12 +4,12 @@ using LumenForgeServer.IntegrationTests.Fixtures;
 using System.Net;
 
 [Collection(AuthCollection.Name)]
-public class HealthTests(ApiHostFixture host)
+public class HealthTests(AuthFixture fixture)
 {
     [Fact]
     public async Task GET_health_returns_ok()
     {
-        var resp = await host.AnonymousClient.GetAsync("/api/v1/health");
+        var resp = await fixture.AnonymousClient.GetAsync("/api/v1/health");
         resp.StatusCode.Should().Be(HttpStatusCode.OK);
     }
 }
